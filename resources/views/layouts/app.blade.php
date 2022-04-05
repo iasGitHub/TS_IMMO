21ea51c295a02f3f031a93c6e1a9b4054627bc9b
ias <idrisssall@yahoo.fr>
2022-04-05 16:40:22 +0000

    change asset by secure_asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,19 +10,19 @@
   <title>Gestion immobilière</title>
 
   <!-- Favicons -->
-  <link rel="icon" href="{{asset('assets/img/favicon.png')}}"/>
-  <link rel="apple-touch-icon" href="{{asset('assets/img/apple-touch-icon.png')}}"/>
+  <link rel="icon" href="{{secure_asset('assets/img/favicon.png')}}"/>
+  <link rel="apple-touch-icon" href="{{secure_asset('assets/img/apple-touch-icon.png')}}"/>
 
   <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" href="{{asset('assets/lib/bootstrap/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('assets/lib/bootstrap/css/bootstrap.min.css')}}">
   <!--external css-->
-  <link rel="stylesheet" href="{{asset('assets/lib/font-awesome/css/font-awesome.css')}}" />
-  <link rel="stylesheet" type="text/css" href="{{asset('assets/css/zabuto_calendar.css')}}">
-  <link rel="stylesheet" type="text/css" href="{{asset('assets/lib/gritter/css/jquery.gritter.css')}}" />
+  <link rel="stylesheet" href="{{secure_asset('assets/lib/font-awesome/css/font-awesome.css')}}" />
+  <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/css/zabuto_calendar.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{secure_asset('assets/lib/gritter/css/jquery.gritter.css')}}" />
   <!-- Custom styles for this template -->
-  <link rel="stylesheet" href="{{asset('assets/css/style.css')}}" >
-  <link rel="stylesheet" href="{{asset('assets/css/style-responsive.css')}}" >
-  <script src="{{asset('assets/lib/chart-master/Chart.js')}}"></script>
+  <link rel="stylesheet" href="{{secure_asset('assets/css/style.css')}}" >
+  <link rel="stylesheet" href="{{secure_asset('assets/css/style-responsive.css')}}" >
+  <script src="{{secure_asset('assets/lib/chart-master/Chart.js')}}"></script>
 
   <!-- =======================================================
     Template Name: Dashio
@@ -50,7 +50,7 @@
         <ul class="nav top-menu">
           <!-- settings start -->
           <li class="dropdown">
-            <a data-toggle="dropdown" class="dropdown-toggle" href="{{asset('assets/index.html#')}}">
+            <a data-toggle="dropdown" class="dropdown-toggle" href="{{secure_asset('assets/index.html#')}}">
               <i class="fa fa-tasks"></i>
               <span class="badge bg-theme">4</span>
               </a>
@@ -60,7 +60,7 @@
                 <p class="green">You have 4 pending tasks</p>
               </li>
               <li>
-                <a href="{{asset('assets/index.html#')}}">
+                <a href="{{secure_asset('assets/index.html#')}}">
                   <div class="task-info">
                     <div class="desc">Dashio Admin Panel</div>
                     <div class="percent">40%</div>
@@ -73,7 +73,7 @@
                 </a>
               </li>
               <li>
-                <a href="{{asset('assets/index.html#')}}">
+                <a href="{{secure_asset('assets/index.html#')}}">
                   <div class="task-info">
                     <div class="desc">Database Update</div>
                     <div class="percent">60%</div>
@@ -86,7 +86,7 @@
                 </a>
               </li>
               <li>
-                <a href="{{asset('assets/index.html#')}}">
+                <a href="{{secure_asset('assets/index.html#')}}">
                   <div class="task-info">
                     <div class="desc">Product Development</div>
                     <div class="percent">80%</div>
@@ -99,7 +99,7 @@
                 </a>
               </li>
               <li>
-                <a href="{{asset('assets/index.html#')}}">
+                <a href="{{secure_asset('assets/index.html#')}}">
                   <div class="task-info">
                     <div class="desc">Payments Sent</div>
                     <div class="percent">70%</div>
@@ -112,14 +112,14 @@
                 </a>
               </li>
               <li class="external">
-                <a href="{{asset('assets/#')}}">See All Tasks</a>
+                <a href="{{secure_asset('assets/#')}}">See All Tasks</a>
               </li>
             </ul>
           </li>
           <!-- settings end -->
           <!-- inbox dropdown start-->
           <li id="header_inbox_bar" class="dropdown">
-            <a data-toggle="dropdown" class="dropdown-toggle" href="{{asset('assets/index.html#')}}">
+            <a data-toggle="dropdown" class="dropdown-toggle" href="{{secure_asset('assets/index.html#')}}">
               <i class="fa fa-envelope-o"></i>
               <span class="badge bg-theme">5</span>
               </a>
@@ -129,8 +129,8 @@
                 <p class="green">You have 5 new messages</p>
               </li>
               <li>
-                <a href="{{asset('assets/index.html#')}}">
-                  <span class="photo"><img alt="avatar" src="{{asset('assets/img/ui-zac.jpg')}}"></span>
+                <a href="{{secure_asset('assets/index.html#')}}">
+                  <span class="photo"><img alt="avatar" src="{{secure_asset('assets/img/ui-zac.jpg')}}"></span>
                   <span class="subject">
                   <span class="from">Zac Snider</span>
                   <span class="time">Just now</span>
@@ -141,8 +141,8 @@
                   </a>
               </li>
               <li>
-                <a href="{{asset('assets/index.html#')}}">
-                  <span class="photo"><img alt="avatar" src="{{asset('assets/img/ui-divya.jpg')}}"></span>
+                <a href="{{secure_asset('assets/index.html#')}}">
+                  <span class="photo"><img alt="avatar" src="{{secure_asset('assets/img/ui-divya.jpg')}}"></span>
                   <span class="subject">
                   <span class="from">Divya Manian</span>
                   <span class="time">40 mins.</span>
@@ -153,8 +153,8 @@
                   </a>
               </li>
               <li>
-                <a href="{{asset('assets/index.html#')}}">
-                  <span class="photo"><img alt="avatar" src="{{asset('assets/img/ui-danro.jpg')}}"></span>
+                <a href="{{secure_asset('assets/index.html#')}}">
+                  <span class="photo"><img alt="avatar" src="{{secure_asset('assets/img/ui-danro.jpg')}}"></span>
                   <span class="subject">
                   <span class="from">Dan Rogers</span>
                   <span class="time">2 hrs.</span>
@@ -165,8 +165,8 @@
                   </a>
               </li>
               <li>
-                <a href="{{asset('assets/index.html#')}}">
-                  <span class="photo"><img alt="avatar" src="{{asset('assets/img/ui-sherman.jpg')}}"></span>
+                <a href="{{secure_asset('assets/index.html#')}}">
+                  <span class="photo"><img alt="avatar" src="{{secure_asset('assets/img/ui-sherman.jpg')}}"></span>
                   <span class="subject">
                   <span class="from">Dj Sherman</span>
                   <span class="time">4 hrs.</span>
@@ -177,14 +177,14 @@
                   </a>
               </li>
               <li>
-                <a href="{{asset('assets/index.html#')}}">See all messages</a>
+                <a href="{{secure_asset('assets/index.html#')}}">See all messages</a>
               </li>
             </ul>
           </li>
           <!-- inbox dropdown end -->
           <!-- notification dropdown start-->
           <li id="header_notification_bar" class="dropdown">
-            <a data-toggle="dropdown" class="dropdown-toggle" href="{{asset('assets/index.html#')}}">
+            <a data-toggle="dropdown" class="dropdown-toggle" href="{{secure_asset('assets/index.html#')}}">
               <i class="fa fa-bell-o"></i>
               <span class="badge bg-warning">7</span>
               </a>
@@ -194,35 +194,35 @@
                 <p class="yellow">You have 7 new notifications</p>
               </li>
               <li>
-                <a href="{{asset('assets/index.html#')}}">
+                <a href="{{secure_asset('assets/index.html#')}}">
                   <span class="label label-danger"><i class="fa fa-bolt"></i></span>
                   Server Overloaded.
                   <span class="small italic">4 mins.</span>
                   </a>
               </li>
               <li>
-                <a href="{{asset('assets/index.html#')}}">
+                <a href="{{secure_asset('assets/index.html#')}}">
                   <span class="label label-warning"><i class="fa fa-bell"></i></span>
                   Memory #2 Not Responding.
                   <span class="small italic">30 mins.</span>
                   </a>
               </li>
               <li>
-                <a href="{{asset('assets/index.html#')}}">
+                <a href="{{secure_asset('assets/index.html#')}}">
                   <span class="label label-danger"><i class="fa fa-bolt"></i></span>
                   Disk Space Reached 85%.
                   <span class="small italic">2 hrs.</span>
                   </a>
               </li>
               <li>
-                <a href="{{asset('assets/index.html#')}}">
+                <a href="{{secure_asset('assets/index.html#')}}">
                   <span class="label label-success"><i class="fa fa-plus"></i></span>
                   New User Registered.
                   <span class="small italic">3 hrs.</span>
                   </a>
               </li>
               <li>
-                <a href="{{asset('assets/index.html#')}}">See all notifications</a>
+                <a href="{{secure_asset('assets/index.html#')}}">See all notifications</a>
               </li>
             </ul>
           </li>
@@ -232,7 +232,7 @@
       </div>
       <div class="top-menu">
         <ul class="nav pull-right top-menu">
-          <li><a class="logout" href="{{asset('assets/login.html')}}">Logout</a></li>
+          <li><a class="logout" href="{{secure_asset('assets/login.html')}}">Logout</a></li>
         </ul>
       </div>
     </header>
@@ -251,9 +251,9 @@
             Buy the pro version with working PHP/AJAX contact form: https://templatemag.com/dashio-bootstrap-admin-template/
             Licensing information: https://templatemag.com/license/
           -->
-          Created with Dashio template by <a href="{{asset('assets/https://templatemag.com/')}}">TemplateMag</a>
+          Created with Dashio template by <a href="{{secure_asset('assets/https://templatemag.com/')}}">TemplateMag</a>
         </div>
-        <a href="{{asset('assets/index.html#')}}" class="go-top">
+        <a href="{{secure_asset('assets/index.html#')}}" class="go-top">
           <i class="fa fa-angle-up"></i>
           </a>
       </div>
