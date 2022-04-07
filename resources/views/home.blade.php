8a90a137c156e7ce5ee8cad48ebf9e053903e7b5
ias <idrisssall@yahoo.fr>
2022-04-07 16:10:34 +0000

    Gallery sur la page d'accueil 3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,24 @@
 @extends('layouts.header')
 
-<div class="row mt">
-          <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
-            <div class="project-wrapper">
-              <div class="project">
-                <div class="photo-wrapper">
-                  <div class="photo">
-                    <a class="fancybox" href="{{secure_asset('assets/img/portfolio/port04.jpg')}}"><img class="img-responsive" src="{{secure_asset('assets/img/portfolio/port04.jpg')}}" alt=""></a>
-                  </div>
-                  <div class="overlay"></div>
+@section('section')
+    <section id="main-content">
+        <section class="wrapper">
+            <div class="row mt">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
+                    <div class="project-wrapper">
+                        <div class="project">
+                            <div class="photo-wrapper">
+                                <div class="photo">
+                                    <a class="fancybox" href="{{secure_asset('assets/img/portfolio/port04.jpg')}}"><img class="img-responsive" src="{{secure_asset('assets/img/portfolio/port04.jpg')}}" alt=""></a>
+                                </div>
+                                <div class="overlay">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-</div>
+        </section>
+    </section>
+@endsection
