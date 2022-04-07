92fc2099b2d3bdf2a67ec4c5e028f95a7dd08bc0
ias <idrisssall@yahoo.fr>
2022-04-07 16:25:30 +0000

    dynamiser images propriétés

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,13 @@
     <section id="main-content">
         <section class="wrapper">
             <div class="row mt">
+                @foreach($proprietes as $propriete)
                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
                     <div class="project-wrapper">
                         <div class="project">
                             <div class="photo-wrapper">
                                 <div class="photo">
-                                    <a class="fancybox" href="{{secure_asset('assets/img/portfolio/port04.jpg')}}"><img class="img-responsive" src="{{secure_asset('assets/img/portfolio/port04.jpg')}}" alt=""></a>
+                                    <a class="fancybox" href="#"><img class="img-responsive" src=/storage{{$propriete->photo }} alt=""></a>
                                 </div>
                                 <div class="overlay">
                                     
@@ -18,6 +19,7 @@
                         </div>
                     </div>
                 </div>
+                @endforeach
             </div>
         </section>
     </section>
