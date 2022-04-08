107a254cd779b5ba75c4b691355b6353b0dfa74d
ias <idrisssall@yahoo.fr>
2022-04-08 16:06:56 +0000

    delete gallery in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,2 @@
 @extends('layouts.header')
 
-@section('section')
-    <section id="main-content">
-        <section class="wrapper">
-            <div class="row mt">
-                @foreach($proprietes as $propriete)
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 desc">
-                    <div class="project-wrapper">
-                        <div class="project">
-                            <div class="photo-wrapper">
-                                <div class="photo">
-                                    <a class="fancybox" href="#"><img class="img-responsive" src="/storage/{{$propriete->photo }}" alt=""></a>
-                                </div>
-                                <div class="overlay">
-                                    
-                                </div>
-                            </div>
-                        </div>  
-                    </div>
-                </div>
-                @endforeach
-            </div>
-        </section>
-    </section>
-@endsection
